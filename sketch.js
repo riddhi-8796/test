be09d053f008de435a4bf9490c6bfa9c9cc473f1
Riddhi Jain <riddhiparmar@gmail.com>
2021-03-10 18:10:33 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform=new Ground(150,305,300,170);
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -43,6 +43,7 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+platform.display
     box1.display();
     box2.display();
     ground.display();
